92cece5a1dd49b188a14cca7fa28b32722c50f08
Mausumi Bhuyan <122201001@smail.iitpkd.ac.in>
2023-05-28 12:23:47 +0530

    first half code

--- test.py
+++ test.py
@@ -8,3 +8,27 @@ ratings = pd.read_csv('ratings.csv')
 
 bookratings = pd.merge(ratings,books,on='bookid')
 musicratings = pd.merge(ratings,books,on='musicid')
+
+bookmatrix = bookratings.pivot_table(index='userid',columns='bookid',values='rating').fillna(0)
+musicmatrix = musicratings.pivot_table(index='userid',columns='musicid',values='rating').fillna(0)
+
+def collaborativefiltering(matrix):
+    similaritymatrix=cosine_similarity(matrix,dense_output=True)
+    recommendations = {}
+    for userindex,userrows in enumerate(matrix):
+        similarusers = similaritymatrix[userindex]
+        userratings = userrows
+        
+        similaruserindices = np.argsort(-similaruser)[1:]
+        similaruserratings = matrix[similaruserindices]
+
+        weightedsum = np.dot(similaruserratings.T,similarusers[1:])
+        weightedaverage = weightedsum/np.sum(np.abs(similarusers[1:]))
+
+        recommendationscores = np.where(userratings == 0,weightedaverage,0)
+
+        topindices = np.argsort(-recommendationscores)[:5]
+
+        recommendations[userindex] = topindices.tolist()
+
+        return recommendations
