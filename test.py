3276819d8cc27623c8f15caf1bde11330071f5af
Mausumi Bhuyan <122201001@smail.iitpkd.ac.in>
2023-05-31 01:50:09 +0530

    game design

--- test.py
+++ test.py
@@ -1,3 +1,113 @@
+def print(cards,conceal):
+    c = ""
+    for i in cards:
+        c = c + "\t ________________"
+    if conceal:
+        c += "\t ________________"
+    print(c)
+
+    c = ""
+    for i in cards:
+        c = c + "\t|                |"
+    if conceal:
+        c += "\t|                |"
+    print(c)
+
+    c = ""
+    for i in cards:
+        if i.value == '10':
+            c = c + "\t|   {}               |".format(i.value)
+        else:
+            c = c + "\t|   {}                |".format(i.value)
+    if conceal:
+        c += "\t|                |"
+    print(c)
+
+    c = ""
+    for i in cards:
+        c = c + "\t|                |"  
+    if conceal:
+        c += "\t|      * *      |"
+    print(c)
+
+    c = ""
+    for i in cards:
+        c = c + "\t|                |"  
+    if conceal:
+        c += "\t|     *   *     |"
+    print(c)
+
+    c = "" 
+    for i in cards:
+        c = c + "\t|               |"  
+    if conceal:
+        c += "\t|    *     *    |"
+    print(c)
+
+    c = ""
+    for i in cards:
+        c = c + "\t|               |"  
+    if conceal:
+        c += "\t|   *       *   |"
+    print(c)
+
+    c = ""
+    for i in cards:
+        c = c + "\t|        {}       |".format(i.shape)
+    if conceal:
+        c += "\t|           *   |" 
+    print(c)
+
+    c = ""
+    for i in cards:
+        c = c + "\t|               |"
+    if conceal:
+        c += "\t|          *    |"
+    print(c)
+
+    c = ""
+    for i in cards:
+        c = c + "\t|               |"
+    if conceal:
+        c += "\t|         *     |"
+    print(c)
+
+    c = "" 
+    for i in cards:
+        c = c + "\t|               |"
+    if conceal:
+        c += "\t|               |"
+    print(c)
+
+    c = "" 
+    for i in cards:
+        c = c + "\t|               |"
+    if conceal:
+        c += "\t|               |"
+    print(c)
+
+    c = ""
+    for i in cards:
+        if i.value == '10':
+            c = c + "\t|           {}        |".format(i.value)
+        else:
+            c = c + "\t|           {}         |".format(i.value)
+    if conceal:
+        c += "\t|        *       |"
+    print(c)
+
+    c = ""
+    for i in cards:
+        c = c + "\t|________________|"
+    if conceal:
+        c += "\t|________________|"
+    print(c)                     
+
+
+                
+
+
+
 
 
 
