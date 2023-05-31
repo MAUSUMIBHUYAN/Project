a3701b007b2cd57b74245eed0d89d3d83e2080ab
PranithaMuluguru <134689526+PranithaMuluguru@users.noreply.github.com>
2023-05-31 16:47:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -218,15 +218,121 @@ def blackjack_game(deck):
             #IF AT ALL THE PLAYER HAS AN ACE CARD WE NEED TO UPDATE THE PLAYER'S SCORE
             m=0
             while player_score>21 and m<len(player_cards):
-                 
-                                                                   
-              
-    
-    
+                 if player_cards[m].Cardvalue == 11:
+                        player_cards[m].cardvalue=1
+                       
+                        player_score -=10
+                        c +=1
+                  else:
+                        c +=1
+             clear_terminal()
+            # displaying player and trader cards
+            print("TRADER CARDS:")
+            print_cards(trader_cards[:-1],True)
+            print("TRADER SCORE  = ",trader_score - trader_cards[-1].Cardvalue)
+            
+            print()
+            
+            print("PLAYER CARDS:")
+            print_cards(player_cards,False)
+            print("PLAYER SCORE =",player_score)
+            
+        # if the player decides to stay and continue
+        if option.upper() =='S'
+             break
+            
+      clear_terminal()
+      
+      
+      # PRINT BOTH PLAYER AND TRAER CARDS
+        print("PLAYER CARDS:")
+        print_cards(player_cards,False)
+        print("PLAYER SCORE:",player_score)
+        
+        print()
+        print("TRADER IS GOING TO SHOW HIS CARDS.......:")
+        print("TRADER CARDS:")
+        
+        print_cards(trader_cards,False)
+        print("TRADER SCORE=",trader_score)
+       # what if the player has a blackjack ....?
+        if player_score == 21
+            print("PLAYER HAS A BLACKJACK)
+            quit()
+       # what if the player busts
+         if player_score > 21:
+             print("PLAYER BUSTED!!! GAME OVERRR!!")
+             quit()
+                  
+          input()
+        
+       # Managing the trader moves
+          while trader_score<17:
+               clear_terminal()
+               print("TRADER DECIDES TO HIT:")
+               
+               # Dealing card for trader
+                trader_card = random.option(deck)
+                trader_cards.append(trader_card)
+                deck.remove(trader_card)
+               #updating the traders score
+                trader_score +=trader_card.Cardvalue
+               # Updating player score in case player's card have ace in them
+                m =0
+                while trader_score >21 and m< len(trader_cards):
+                     if trader_cards[m].Cardvalue ==11:
+                        trader_cards[m].Cardvalue = 1
+                        trader_score -= 10
+                        c +=1
+                      
+                      else:
+                         c +=1
+                 #PRINT PLAYER NAD TRADER CARDS
+                  print("PLAYER CARDS:")
+                  print_cards(player_cards,False)
+                  print("PLAYER SCORE =",player_score)
+                  print()
+                  
+                  print("TRADER CARDS:")
+                  print_cards(trader_cards,False)
+                  print("TRADER SCORE =",dealer_score)
+                  
+                  input()
+       # WHAT IF TRADER BUSTS
+        if trader_score >21:
+                   print("TRADER BUSTED!!! PLAYER WINS!!!)
+                   quit()
+       # WHAT IF TRADER GETS A BLACKJACK
+        if trader_score ==21:
+                    print("TRADER WINSS!!! ")
+                    quit()
+       #TIE
+         if trader_score == player_score:
+                    print("TIE GAMEE!!!")
+       #PLAYER WINSS IF
+         if player_score > trader_score:
+                     print("PLAYER WINS!!!")
+       # TRAER WINS IF
+         else:
+                     print("TRADER WINS!!!")
+         
+if __name__ == '__main__':
+        # the type of shape
+         shape=["Spades","Hearts","Clubs","Diamonds"]
+        #the suit value
+          suit value ={"Spades":"\u2664","Hearts":"\u2661","Clubs":"\u2667","Diamonds":"\u2662"}
+        #the type of card
+          cards=["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+        # the card values
+           Cardvalues={"A":11,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":10,"Q":10,"K":10}
+        # the deck of cards
+           deck=[]
+        # loop for every type of shape
+           for shape in shapes:
+               # loopfor every type of card in shape
+                    for card in shape:
+                         #add card to deck
+                         deck.append(Card(suits_values[shape],card,cards_values[cards]))
 
-
-                
-
-
-
+           blackjack_game(deck)
 
