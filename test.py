d2b1c3167963dc0bdca3d222bf2832e08b92c8eb
Mausumi Bhuyan <122201001@smail.iitpkd.ac.in>
2023-06-02 11:11:36 +0530

    modify 2

--- test.py
+++ test.py
@@ -1,337 +1,180 @@
 import random
-import os
-#defining the card class
-class Card:
-    def __init__(self,shape,value,Cardvalue):
-        # shape of the card 
+
+shapes = ('Spades','Diamonds','Hearts','Clubs')
+ranks = ('2','3','4','5','6','7','8','9','Ace','Jack','Queen','King')
+values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7':7, '8':8, '9':9, 'Ace':11, 'Jack':11, 'Queen':12, 'King':13, }
+
+#when the game is playing properly not the whole loop that makes the whole game
+playing = True
+#define Classes
+class Cards: #creates all the cards
+    def __init__(self,shape,rank):#to add attributes
         self.shape = shape
-        # representing value of the card
-        self.value = value
-        # Cardvalue of the card
-        self.Cardvalue = Cardvalue
-def clear_terminal():
-    os.system('cls' if os.name == 'nt' else 'clear')
-def print_cards(cards,conceal):
-    c = ""
-    for i in cards:
-        c = c + "\t ________________"
-    if conceal:
-        c += "\t ________________"
-    print(c)
+        self.rank = rank
+    def __str__(self):
+        return self.rank + ' of ' + self.shape# return like Queen of hearts
+class Deck:#create a deck of cards
+    def __init__(self):
+        self.deck = [] # haven't created a deck
+        for shape in shapes:
+            for rank in ranks:
+                self.deck.append(Cards(shape,rank))#to create a nice stack and all deck is going to be like a whole string
 
-    c = ""
-    for i in cards:
-        c = c + "\t|                |"
-    if conceal:
-        c += "\t|                |"
-    print(c)
+    def __str__(self):
+        combination_of_deck = ''
+        for card in self.deck:
+            combination_of_deck += '\n' + card.__str__()#my combination of deck is equal to my deck which can't plus the card so a new line and a card is equal to deckcomination and setting the value again so reassigning it every time
+        return 'The deck has : ' +combination_of_deck
 
-    c = ""
-    for i in cards:
-        if i.value == '10':
-            c = c + "\t|   {}               |".format(i.value)
+    def shuffle(self): #shuffles the card in the deck
+        random.shuffle(self.deck)
+
+    def pick(self): #pick the card from the deck
+        #pop means takeoff the lastvalue from a list or a string        
+        pickout_card = self.deck.pop()#the pickout card is going to be the card that is popped off from the end of the deck
+        return pickout_card
+test = Deck()
+print(test)
+
+class Have: #shows all the cards that the player and the opponent have 
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0 #keep track of aces
+    def add_card(self, card): #add a card to the player as well as the opponent
+        self.cards.append(card)
+        self.value += values[card.rank] #calculating the values of the cards
+        if card.rank == 'Ace':
+            self.aces += 1
+
+    def ace(self):#if the sum is really close to 21 we'll just adjust make the ace turn into 1
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+class Bets: # to keep track of all the bets 
+    def __init__(self):
+        #when the user lost their bet,the amount they bet was taken away from the self.total
+        self.total = 0
+        self.bet = 0
+
+    def win_the_bet(self):
+        self.total += self.bet
+
+    def lost_the_bet(self):
+        self.total -= self.bet
+#functions
+def take_the_bet(amount):
+
+    while True:
+        try:
+            amount.bet = int(input("Enter the amount you want to bet:"))
+        except ValueError:
+            print("Excuse me! Can you type in a number:")
         else:
-            c = c + "\t|   {}                |".format(i.value)
-    if conceal:
-        c += "\t|                |"
-    print(c)
-
-    c = ""
-    for i in cards:
-        c = c + "\t|                |"  
-    if conceal:
-        c += "\t|      * *      |"
-    print(c)
-
-    c = ""
-    for i in cards:
-        c = c + "\t|                |"  
-    if conceal:
-        c += "\t|     *   *     |"
-    print(c)
-
-    c = "" 
-    for i in cards:
-        c = c + "\t|               |"  
-    if conceal:
-        c += "\t|    *     *    |"
-    print(c)
-
-    c = ""
-    for i in cards:
-        c = c + "\t|               |"  
-    if conceal:
-        c += "\t|   *       *   |"
-    print(c)
-
-    c = ""
-    for i in cards:
-        c = c + "\t|        {}       |".format(i.shape)
-    if conceal:
-        c += "\t|           *   |" 
-    print(c)
-
-    c = ""
-    for i in cards:
-        c = c + "\t|               |"
-    if conceal:
-        c += "\t|          *    |"
-    print(c)
-
-    c = ""
-    for i in cards:
-        c = c + "\t|               |"
-    if conceal:
-        c += "\t|         *     |"
-    print(c)
-
-    c = "" 
-    for i in cards:
-        c = c + "\t|               |"
-    if conceal:
-        c += "\t|               |"
-    print(c)
-
-    c = "" 
-    for i in cards:
-        c = c + "\t|               |"
-    if conceal:
-        c += "\t|               |"
-    print(c)
-
-    c = ""
-    for i in cards:
-        if i.value == '10':
-            c = c + "\t|           {}        |".format(i.value)
-        else:
-            c = c + "\t|           {}         |".format(i.value)
-    if conceal:
-        c += "\t|        *       |"
-    print(c)
-
-    c = ""
-    for i in cards:
-        c = c + "\t|________________|"
-    if conceal:
-        c += "\t|________________|"
-    print(c)   
-    print()
-    
-    # creating a function for the blackjack game (single person)
-def Blackjack_game(deck):
-    #cards for both trader and player
-    player_cards=[]
-    trader_cards=[]
-    
-    # to write the total Scores for player and trader
-    player_score=[]
-    trader_score=[]
-    
-    clear_terminal()
-    
-    #condition for the trading the card
-    while len(player_cards)<2:
-
-        #choosing any card
-        player_card=random.choice(deck)
-        player_cards.append(player_card)
-        deck.remove(player_card)
-        
-        #to update the total scores of the player
-        player_score= player_score + [player_card.Cardvalue]
-        
-        #in case both the cards are Ace, make the first ace value as 1
-        if len(player_cards)==2:
-               if player_cards[0].Cardvalue == 11 and player_cards[1].Cardvalue == 11:
-                    player_cards[0].Cardvalue=1
-                    player_score -= 10
-                    
-        # print players cards and score
-        print("PLAYER CARDS:")
-        print_cards(player_cards,False)
-        print("PLAYER SCORE=",player_score)
-         
-        input()
-        
-        #choosing any card
-        trader_card= random.choice(deck)
-        trader_cards.append(trader_card)
-        deck.remove(trader_card)
-
-        # to update the total scores of the trader
-        trader_score= trader_score+[trader_card.Cardvalue]
-
-        #to print the scores and cards of the trader
-        print("TRADER'S CARDS:")      
-        if len(trader_cards)==1:
-              print_cards(trader_cards,False)
-              print("THE TRADER'S SCORE IS=",trader_score)
-        else :
-              print_cards(trader_cards[:-1],True)
-              print("THE TRADER'S SCORE IS=",trader_score-trader_cards[-1].Card_value)
-
-        # in case both the cards are Ace,make the second ace value as 1
-        if len(trader_cards==2):
-              if  trader_cards[0].Cardvalue ==11 and trader_cards[1].Cardvalue == 11:
-                  trader_cards[1].Cardvalue = 1
-                  trader_score = trader_score+10
-
-        input()
-    #  necessary condition for the player to get blackjack
-    if player_score ==21:
-        print("PLAYER GOT THE BLACKJACK!!!!") 
-        print("PLAYER WINS!!!")
-        quit()
-    clear_terminal()
-
-    # to print the player and trader cards
-    print("TRADER CARDS ARE:")
-    print_cards(trader_cards[:-1], True)
-    print("TRADER'S SCORE=" , trader_score-trader_cards[-1].Cardvalue)
-
-    print()
-
-    print("PLAYER CARDS ARE:")
-    print_cards(player_cards, False)
-    print("PLAYER'S SCORE:", player_score)
-
-    #to control the players and traders moves
-    while player_score <21:
-        option = input("ENTER H for HIT or S for STAND:")
-
-        if len(option)!=1 or (option.upper() != 'H' and option.upper()!= 'S'):
-            clear_terminal()
-            print ("INVALID INPUT")
-
-        # IF THE PERSON CHOOSES HIT(H)
-        if option.upper()== 'H':
-
-            #IF WE HAVE A NEW CARD
-            player_card = random.option(deck)
-            player_cards.append(player_card)
-            deck.remove(player_card)
-
-            #TO ADD UP THE PREVIOUS SCORES
-            player_score= player_score+ player_card.Cardvalue
-
-            #IF AT ALL THE PLAYER HAS AN ACE CARD WE NEED TO UPDATE THE PLAYER'S SCORE
-            m=0
-            while player_score>21 and m<len(player_cards):
-                if player_cards[m].Cardvalue == 11:
-                        player_cards[m].Cardvalue=1
-                       
-                        player_score -=10
-                        c +=1
-                else:
-                        c +=1
-            clear_terminal()
-            # displaying player and trader cards
-            print("TRADER CARDS:")
-            print_cards(trader_cards[:-1],True)
-            print("TRADER SCORE  = ",trader_score - trader_cards[-1].Cardvalue)
-            
-            print()
-            
-            print("PLAYER CARDS:")
-            print_cards(player_cards,False)
-            print("PLAYER SCORE =",player_score)
-            
-        # if the player decides to stay and continue
-        if option.upper() =='S':
-            break
-            
-    clear_terminal()
-      
-      
-      # PRINT BOTH PLAYER AND TRAER CARDS
-    print("PLAYER CARDS:")
-    print_cards(player_cards,False)
-    print("PLAYER SCORE:",player_score)
-        
-    print()
-    print("TRADER IS GOING TO SHOW HIS CARDS.......:")
-    print("TRADER CARDS:")
-        
-    print_cards(trader_cards,False)
-    print("TRADER SCORE=",trader_score)
-    # what if the player has a blackjack ....?
-    if player_score == 21 :
-        print("PLAYER HAS A BLACKJACK")
-        quit()
-    # what if the player busts
-    if player_score > 21:
-        print("PLAYER BUSTED!!! GAME OVERRR!!")
-        quit()
-                  
-    input()
-        
-    # Managing the trader moves
-    while trader_score<17:
-        clear_terminal()
-        print("TRADER DECIDES TO HIT")
-               
-        # Dealing card for trader
-        trader_card = random.option(deck)
-        trader_cards.append(trader_card)
-        deck.remove(trader_card)
-        #updating the traders score
-        trader_score +=trader_card.Cardvalue
-        # Updating player score in case player's card have ace in them
-        m =0
-        while trader_score >21 and m< len(trader_cards):
-            if trader_cards[m].Cardvalue ==11:
-                trader_cards[m].Cardvalue = 1
-                trader_score -= 10
-                c +=1
-                      
+            if amount.bet < 100:
+                print("Your bet should not be less than 100!") 
             else:
-                c +=1
-        #PRINT PLAYER NAD TRADER CARDS
-        print("PLAYER CARDS:")
-        print_cards(player_cards,False)
-        print("PLAYER SCORE =",player_score)
-        print()
-                  
-        print("TRADER CARDS:")
-        print_cards(trader_cards,False)
-        print("TRADER SCORE =",trader_score)
-                  
-        input()
-    # WHAT IF TRADER BUSTS
-    if trader_score >21:
-        print("TRADER BUSTED!!! PLAYER WINS!!!")
-        quit()
-    # WHAT IF TRADER GETS A BLACKJACK
-    if trader_score ==21:
-        print("TRADER WINSS!!! ")
-        quit()
-    #TIE
-    if trader_score == player_score:
-        print("TIE GAMEE!!!")
-    #PLAYER WINSS IF
-    if player_score > trader_score:
-        print("PLAYER WINS!!!")
-    # TRAER WINS IF
-    else:
-        print("TRADER WINS!!!")
-         
-if __name__ == '__main__':
-        # the type of shape
-        Shapes=["Spades","Hearts","Clubs","Diamonds"]
-        #the suit value
-        Shapesvalues={"Spades":"\u2664","Hearts":"\u2661","Clubs":"\u2667","Diamonds":"\u2662"}
-        #the type of card
-        cards=["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-        # the card values
-        Cardvalues={"A":11,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":11,"Q":12,"K":13}
-        # the deck of cards
-        Deck=[]
-        # loop for every type of shape
-        for shape in Shapes:
-            # loopfor every type of card in shape
-            for i in cards:
-            #add card to deck
-                Deck.append(Card(Shapesvalues[shape],i,Cardvalues[i]))
+                break
+def hit(deck, have):
+    have.add_card(deck.pick())
+    have.ace()#in case the card is an ace
 
-        Blackjack_game(Deck)
+def hit_or_stand(deck, have):
+    global playing#making it available  in the function
+
+    while True:
+        ask = input("Would you like to hit or stand: ")
+
+        if ask.lower() == 'hit':
+            hit(deck, have)
+        elif ask.lower() == 'stand':
+            print("Player choose to stand, the ooponent start playing.")
+            playing = False#it won't stop the whole loop of the game but it will stop the player from doing anything else while the dealer is playing
+        else:
+            print("you can either type hit or stand.")
+            continue
+        break#it will break if the answer is correct
+
+def show_the_card(player, opponent):
+    print("\nOpponent have: ")
+    print("card is hidden")
+    print("",opponent.cards[1])
+    print("\nplayer have: ", *player.cards, sep='\n')#separate the cards together and separate it with new line
+    
+def show_card(player, opponent):
+    print("\nOpponent have: ", *opponent.cards, sep='\n')
+    print("Opponent have=", opponent.value)
+    print("\nPlayer have: ", *player.cards, sep='\n')
+    print("Player have=", player.value)
+
+#game endings
+def player_lose(player, opponent, amount):
+    print("OHH,YOU LOST THE GAME!")
+    amount.lost_the_bet()
+def player_win(player, opponent, amount):
+    print("BRAVO,YOU WON THE GAME!")
+    amount.win_the_bet()
+def opponent_lose(player, opponent, amount):
+    print("OHH,YOU LOST THE GAME!")
+    amount.lost_the_bet()
+def opponent_win(player, opponent, amount):
+    print("BRAVO,YOU WON THE GAME!")
+    amount.win_the_bet()
+
+def Tie(player, opponent):#if the player and the opponent tie
+    print("Its a tie!")
+
+#build the game
+while True:
+    print("Welcome to Blackjack game!")
+
+    #create an shuffle deck
+    deck = Deck()
+    deck.shuffle()
+
+    player_have = Have()
+    player_have.add_card(deck.pick())#add the card twice
+    player_have.add_card(deck.pick())
+
+    opponent_have = Have()
+    opponent_have.add_card(deck.pick())#add the card twice
+    opponent_have.add_card(deck.pick())
+
+    #set up the amount
+    player_bet = Bets()
+
+    #ask player for bet
+    take_the_bet(player_bet)
+    #show cards
+    show_the_card(player_have, opponent_have)
+    
+    while playing:
+        hit_or_stand(deck, player_have)
+        show_the_card(player_have, opponent_have)
+
+        if player_have.value > 21:
+            player_lose(player_have, opponent_have, player_bet)
+    if player_have.value <= 21:
+        while opponent_have.value < 17:
+            hit(deck, opponent_have)
+        show_card(player_have, opponent_have)    
+        points = {}
+        points[player_have.value] = 21 - player_have.value
+        points[opponent_have.value] = 21 - opponent_have.value
+        if points[player_have.value] > points[opponent_have.value]:
+            player_lose(player_have, opponent_have, player_bet)
+        elif points[player_have.value] == points[opponent_have.value]:
+            Tie(player_have, opponent_have)
+        else:
+            player_win(player_have, opponent_have, player_bet)    
+    
+    print("\nPlayer's winnings stand at", player_bet.total)
+
+    new_game = input("Would you like to play again?")
+    if new_game.lower() == 'yes':
+        playing = True
+        continue
+    else:
+        print("Thanks for playing the Blackjack game!")
+        break
 
