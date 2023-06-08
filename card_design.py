8b3a444b49adef87fdc2ab1d3da7bc4945b58994
AnjithaVinod <134689259+AnjithaVinod@users.noreply.github.com>
2023-06-08 19:14:47 +0530

    Add files via upload

---
+++ card_design.py
@@ -0,0 +1,5 @@
+print('\u2500' * 9)  # to make the straight line
+
+print('\u250c─────────\u2510')  # to make the upper part border of the card
+
+print('\u2514─────────\u2518')  # to make the lower part border of the card
