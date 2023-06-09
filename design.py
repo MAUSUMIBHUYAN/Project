0ece7509562bc96c5c74346817e1ccd79aaca1ff
Mausumi Bhuyan <134128566+MAUSUMIBHUYAN@users.noreply.github.com>
2023-06-09 22:48:17 +0530

    design.py

--- design.py
+++ design.py
@@ -226,7 +226,7 @@ while True:
         elif player_have.value > opponent_have.value:
             player_win(player_have, opponent_have, player_bet)
         elif player_have.value < opponent_have.value:
-            opponent_win(player_have, opponent_have)
+            opponent_win(player_have, opponent_have, player_bet)
         else:
             Tie(player_have, opponent_have)
 
@@ -470,7 +470,7 @@ while True:
         elif player_have.value > opponent_have.value:
             player_win(player_have, opponent_have, player_bet)
         elif player_have.value < opponent_have.value:
-            opponent_win(player_have, opponent_have)
+            opponent_win(player_have, opponent_have, player_bet)
         else:
             Tie(player_have, opponent_have)
 
